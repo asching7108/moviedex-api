6d16d975666bada9462ec7c1204b64ebf1fefeed
asching7108 <asching7108@gmail.com>
2020-01-18 18:11:12 -0500

    Complete server by Express

---
+++ server.js
@@ -0,0 +1,60 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const MOVIEDEX = require('./movies-data.json');
+const app = express();
+
+// middlewares
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+    }
+    next();
+});
+
+app.get('/movies', (req, res) => {
+    let response = MOVIEDEX;
+    const { genre, country, avg_vote } = req.query;
+
+    // query validation
+    if (avg_vote && isNaN(parseFloat(avg_vote))) {
+        return res
+            .status(400)
+            .send('avg_vote must be a numeric value');
+    }
+
+    // filters response to include the given genre
+    if (genre) {
+        response = response.filter(movie => 
+            movie.genre.toLowerCase().includes(genre.toLowerCase())
+        );
+    }
+
+    // filters response to include the given country
+    if (country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(country.toLowerCase())
+        );
+    }
+
+    // filters response to be greater than or equal to the given avg_vote
+    if (avg_vote) {
+        response = response.filter(movie => 
+            movie.avg_vote >= Number(avg_vote)
+        );
+    }
+
+    res.json(response);
+});
+
+app.listen(8000, () => {
+    console.log('Server started on PORT 8000');
+});
